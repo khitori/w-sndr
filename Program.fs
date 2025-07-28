1671e92de6184d0681dc660f0a26619ec2d17d43
KHITORI <138244861+khitori@users.noreply.github.com>
2025-07-28 08:42:46 +0300

    upload

---
+++ Program.fs
@@ -0,0 +1,143 @@
+﻿open WindowInterop
+open System
+open System.IO
+open System.Runtime.InteropServices
+open System.Diagnostics
+open System.Reflection
+
+
+
+let hotkeyUseEvent () =
+    let cursorMon = getCursorMonitor ()
+    let isPrimary = isPrimaryMonitor cursorMon
+    printfn "Cursor on %s monitor" (if isPrimary then "main" else "other")
+
+    let primaryWins = ResizeArray()
+    let windows = getAllWindows ()
+
+    windows
+    |> Array.iter (fun (hWnd, _) ->
+        if isWindowOnPrimaryMonitor hWnd then
+            primaryWins.Add hWnd
+            printfn "win on primary"
+        else
+            printfn "win on other")
+
+    match primaryWins.Count with
+    | 1 -> 
+    | 2 ->
+    | 3 ->
+    | 4 ->
+    | 5 ->
+    | 6 ->
+
+
+
+
+module NativeMethods =
+    [<DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)>]
+    extern IntPtr SetWindowsHookEx(int idHook, IntPtr lpfn, IntPtr hMod, uint dwThreadId)
+
+    [<DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)>]
+    extern bool UnhookWindowsHookEx(IntPtr hhk)
+
+    [<DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)>]
+    extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam)
+
+    [<DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)>]
+    extern int GetMessage(IntPtr& lpMsg, IntPtr hWnd, uint wMsgFilterMin, uint wMsgFilterMax)
+
+    [<DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)>]
+    extern bool TranslateMessage(IntPtr& lpMsg)
+
+    [<DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)>]
+    extern bool DispatchMessage(IntPtr& lpMsg)
+
+    [<DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)>]
+    extern IntPtr GetModuleHandle(string lpModuleName)
+
+let WH_KEYBOARD_LL = 13
+let WM_KEYDOWN = 0x0100
+let WM_KEYUP = 0x0101
+let WM_SYSKEYDOWN = 0x0104 // SysKeys like Alt
+let WM_SYSKEYUP = 0x0105
+let VK_LMENU = 0xA4 // Left Alt
+let VK_RMENU = 0xA5 // Right Alt
+let VK_X = 0x58 // X Key
+
+type LowLevelKeyboardProc = delegate of int * IntPtr * IntPtr -> IntPtr
+
+let mutable isAltPressed = false
+
+let hookCallback (nCode: int) (wParam: IntPtr) (lParam: IntPtr) : IntPtr =
+    if nCode >= 0 then
+        let vkCode = Marshal.ReadInt32 lParam
+        //printfn "nCode: %d, wParam: %A, vkCode: %X" nCode wParam vkCode
+
+        if wParam = IntPtr WM_KEYDOWN || wParam = IntPtr WM_SYSKEYDOWN then
+            if vkCode = VK_LMENU || vkCode = VK_RMENU then
+                isAltPressed <- true
+            //printfn "Alt pressed, isAltPressed: %b" isAltPressed
+            elif vkCode = VK_X && isAltPressed then
+                hotkeyUseEvent ()
+        //printfn "Hotkey Alt+X pressed"
+        elif wParam = IntPtr WM_KEYUP || wParam = IntPtr WM_SYSKEYUP then
+            if vkCode = VK_LMENU || vkCode = VK_RMENU then
+                isAltPressed <- false
+    //printfn "Alt released, isAltPressed: %b" isAltPressed
+
+    NativeMethods.CallNextHookEx(IntPtr.Zero, nCode, wParam, lParam)
+
+let setHook (proc: LowLevelKeyboardProc) =
+    let hMod = NativeMethods.GetModuleHandle null
+    NativeMethods.SetWindowsHookEx(WH_KEYBOARD_LL, Marshal.GetFunctionPointerForDelegate proc, hMod, 0u)
+
+let runMessageLoop () =
+    let mutable msg = IntPtr.Zero
+
+    while NativeMethods.GetMessage(&msg, IntPtr.Zero, 0u, 0u) <> 0 do
+        NativeMethods.TranslateMessage(&msg) |> ignore
+        NativeMethods.DispatchMessage(&msg) |> ignore
+
+
+
+
+
+[<EntryPoint>]
+let main _ =
+
+    let hookDelegate = LowLevelKeyboardProc hookCallback
+    let hookId = setHook hookDelegate
+
+
+
+    let windows = getAllWindows ()
+    printfn "Найдено %d окон" windows.Length
+
+    windows |> Array.iteri (fun i (_, title) -> printfn "[%d] %s" i title)
+
+    if windows.Length > 0 then
+        let hWnd, title = windows[0]
+        printfn "\nРаботаем с: %s\n" title
+    //moveWindow hWnd (100, 100, 800, 600)
+    //minimizeWindow hWnd
+    //System.Threading.Thread.Sleep 1000
+    //restoreWindow hWnd
+
+    // let cursorMon = getCursorMonitor ()
+    // let isPrimary = isPrimaryMonitor cursorMon
+    // printfn "Cursor on %s monitor" (if isPrimary then "main" else "other")
+
+    // let hwnd, _ = getAllWindows()[0]
+    // let winMon = getWindowMonitor hwnd
+    // printfn "Window on %s monitor" (if isPrimaryMonitor winMon then "main" else "other")
+
+
+
+    if hookId = IntPtr.Zero then
+        printfn "Failed to set hook"
+        1
+    else
+        runMessageLoop ()
+        NativeMethods.UnhookWindowsHookEx hookId |> ignore
+        0
---
+++ WindowInterop.fs
@@ -0,0 +1,145 @@
+module WindowInterop
+
+open System
+open System.Text
+open System.Runtime.InteropServices
+
+
+
+
+
+[<StructLayout(LayoutKind.Sequential)>]
+type RECT =
+    struct
+        val mutable Left: int
+        val mutable Top: int
+        val mutable Right: int
+        val mutable Bottom: int
+    end
+
+type ShowWindowCmd =
+    | Hide = 0
+    | ShowNormal = 1
+    | Minimize = 6
+    | Restore = 9
+
+type EnumWindowsProc = delegate of IntPtr * IntPtr -> bool
+
+[<DllImport("user32.dll")>]
+extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam)
+
+[<DllImport("user32.dll")>]
+extern bool IsWindowVisible(IntPtr hWnd)
+
+[<DllImport("user32.dll")>]
+extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount)
+
+[<DllImport("user32.dll")>]
+extern int GetWindowTextLength(IntPtr hWnd)
+
+[<DllImport("user32.dll")>]
+extern bool GetWindowRect(IntPtr hWnd, RECT& lpRect)
+
+[<DllImport("user32.dll")>]
+extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint)
+
+[<DllImport("user32.dll")>]
+extern bool ShowWindow(IntPtr hWnd, ShowWindowCmd nCmdShow)
+
+let getAllWindows () =
+    let results = ResizeArray()
+
+    let callback =
+        EnumWindowsProc(fun hWnd _ ->
+            if IsWindowVisible hWnd && GetWindowTextLength hWnd > 0 then
+                let sb = StringBuilder 256
+
+                if GetWindowText(hWnd, sb, sb.Capacity) > 0 then
+                    results.Add(hWnd, sb.ToString())
+
+            true)
+
+    EnumWindows(callback, IntPtr.Zero) |> ignore
+    results.ToArray()
+
+let moveWindow hWnd (x, y, w, h) =
+    MoveWindow(hWnd, x, y, w, h, true) |> ignore
+
+let minimizeWindow hWnd =
+    ShowWindow(hWnd, ShowWindowCmd.Minimize) |> ignore
+
+let restoreWindow hWnd =
+    ShowWindow(hWnd, ShowWindowCmd.Restore) |> ignore
+
+
+// MONITOR -----------------------------------------------------------------------------------------------
+
+[<StructLayout(LayoutKind.Sequential)>]
+type POINT =
+    struct
+        val mutable X: int
+        val mutable Y: int
+    end
+
+[<StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)>]
+type MONITORINFO =
+    struct
+        val mutable cbSize: uint32
+        val mutable rcMonitor: RECT
+        val mutable rcWork: RECT
+        val mutable dwFlags: uint32
+    end
+
+[<DllImport("user32.dll")>]
+extern nativeint MonitorFromWindow(nativeint hwnd, uint32 dwFlags)
+
+[<DllImport("user32.dll")>]
+extern nativeint MonitorFromPoint(POINT pt, uint32 dwFlags)
+
+[<DllImport("user32.dll")>]
+extern bool GetMonitorInfo(nativeint hMonitor, MONITORINFO& lpmi)
+
+[<DllImport("user32.dll")>]
+extern bool GetCursorPos(POINT& lpPoint)
+
+let getCursorMonitor () =
+    let mutable pt = POINT()
+
+    if GetCursorPos(&pt) then
+        MonitorFromPoint(pt, 2u) // MONITOR_DEFAULTTONEAREST
+    else
+        IntPtr.Zero
+
+let getWindowMonitor (hWnd: nativeint) = MonitorFromWindow(hWnd, 2u) // MONITOR_DEFAULTTONEAREST
+
+let isPrimaryMonitor (hMonitor: nativeint) =
+    let mutable mi = MONITORINFO(cbSize = uint32 (Marshal.SizeOf<MONITORINFO>()))
+
+    if GetMonitorInfo(hMonitor, &mi) then
+        mi.dwFlags &&& 1u = 1u // MONITORINFOF_PRIMARY = 0x00000001
+    else
+        false
+
+let isWindowOnPrimaryMonitor (hWnd: nativeint) =
+    let MONITOR_DEFAULTTONEAREST = 2u
+    let hMonitor = MonitorFromWindow(hWnd, MONITOR_DEFAULTTONEAREST)
+    isPrimaryMonitor hMonitor
+
+let getMonitorResolutionAndWorkArea (hMonitor: nativeint) =
+    let mutable mi = MONITORINFO(cbSize = uint32 (Marshal.SizeOf<MONITORINFO>()))
+
+    if GetMonitorInfo(hMonitor, &mi) then
+        let fullWidth = mi.rcMonitor.Right - mi.rcMonitor.Left
+        let fullHeight = mi.rcMonitor.Bottom - mi.rcMonitor.Top
+
+        let workWidth = mi.rcWork.Right - mi.rcWork.Left
+        let workHeight = mi.rcWork.Bottom - mi.rcWork.Top
+
+        (fullWidth, fullHeight), (workWidth, workHeight)
+    else
+        (0, 0), (0, 0)
+
+let getWindowMonitorResolutionAndWorkArea (hWnd: nativeint) =
+    let MONITOR_DEFAULTTONEAREST = 2u
+    let hMonitor = MonitorFromWindow(hWnd, MONITOR_DEFAULTTONEAREST)
+    getMonitorResolutionAndWorkArea hMonitor
